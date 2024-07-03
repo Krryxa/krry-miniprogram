bb523596944d827db5b22cc50902f60d0cdb01b5
Krryxa <krryxq@163.com>
2024-07-03 16:56:31 +0800

    feat: 自定义导航栏

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 App({
   onLaunch() {
+    this.setNavBarInfo()
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -15,5 +16,21 @@ App({
   },
   globalData: {
     userInfo: null
+  },
+
+  /**
+   * @description 设置导航栏信息
+   */
+  setNavBarInfo () {
+    // 获取系统信息
+    const systemInfo = wx.getSystemInfoSync();
+    // 胶囊按钮位置信息
+    const menuButtonInfo = wx.getMenuButtonBoundingClientRect();
+    // 导航栏高度 = 状态栏到胶囊的间距（胶囊距上距离-状态栏高度） * 2 + 胶囊高度 + 状态栏高度
+    this.globalData.navBarHeight = (menuButtonInfo.top - systemInfo.statusBarHeight) * 2 + menuButtonInfo.height + systemInfo.statusBarHeight;
+    this.globalData.statusBarHeight = systemInfo.statusBarHeight; // 状态栏高度
+    this.globalData.menuBotton = menuButtonInfo.top - systemInfo.statusBarHeight;
+    this.globalData.menuRight = systemInfo.screenWidth - menuButtonInfo.right;
+    this.globalData.menuHeight = menuButtonInfo.height;
   }
 })
